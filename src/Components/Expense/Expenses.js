b008d8fa063d54da9d0f9785be779d89a74f8507
PatilKavya <kavyapatilgonalst@gmail.com>
2023-01-13 21:56:17 +0530

    delete Components

--- src/Components/Expense/Expenses.js
+++ src/Components/Expense/Expenses.js
@@ -1,13 +1,20 @@
 import ExpenseItem from "./ExpenseItem";
 import Card from '../UI/Card'
 import './Expenses.css' 
+import React from "react";
 
 const Expenses = (props) => {
+      const deleteHandler = (e) =>{
+            const item=e.target.parentElement
+           item.remove();
+        
+      }
     return  (
     <Card className="expenses">
-         <ExpenseItem  datee={props.date}
+         <ExpenseItem datee={props.date}
             titlee={props.title}
             pricee={props.price}></ExpenseItem>
+            <button onClick={deleteHandler}>Delete Expence</button>
       </Card>
       )
 }
