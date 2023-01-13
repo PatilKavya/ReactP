9eb0fba3a45b0fca32aedc7a1f3b600bde19867d
PatilKavya <kavyapatilgonalst@gmail.com>
2023-01-13 09:50:33 +0530

    toLocaleString

--- src/Components/ExpenceDate.js
+++ src/Components/ExpenceDate.js
@@ -1,8 +1,8 @@
 import "./ExpenceDate.css";
 
 function ExpenseDate(props) {
-  const month = props.date.getMonth();
-  const day = props.date.getDay();
+  const month = props.date.toLocaleString('en-US', { month: 'long' });
+  const day = props.date.toLocaleString('en-US', { day: '2-digit'});
   const year = props.date.getFullYear();
 
   return (
