254f112e3f52e3793a19656a622840d6d50760c2
PatilKavya <kavyapatilgonalst@gmail.com>
2023-01-18 12:26:48 +0530

    showing data on screen

--- src/Components/NewExpense/ExpenseForm.js
+++ src/Components/NewExpense/ExpenseForm.js
@@ -1,60 +1,80 @@
-import React, {useState} from 'react'
-import './ExpenseForm.css'
+import React, { useState } from "react";
+import "./ExpenseForm.css";
 
-export default function ExpenceForm() {
+export default function ExpenceForm(props) {
 
-  const [enteredTitle,setEnteredTitle] = useState('')
-  const [enteredAmount,setEnteredAmount] = useState('')
-  const [enteredDate,setEnteredDate] = useState('')
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAmount, setEnteredAmount] = useState("");
+  const [enteredDate, setEnteredDate] = useState("");
 
-    const titleChangeHandler=(event)=>{
-      setEnteredTitle(event.target.value)
-}
+  const titleChangeHandler = (event) => {
+    setEnteredTitle(event.target.value);
+  };
 
-const amountChangeHandler=(event)=>{
-  setEnteredAmount(event.target.value)
-  }
+  const amountChangeHandler = (event) => {
+    setEnteredAmount(event.target.value);
+  };
 
-  const dateChangeHandler=(event)=>{
-    setEnteredDate(event.target.value)
-    }
+  const dateChangeHandler = (event) => {
+    setEnteredDate(event.target.value);
+  };
 
-    const expenceCreate = (event) =>{
-      event.preventDefault()
-      const expenses={title:enteredTitle,amount:enteredAmount,date:new Date(enteredDate)}
-      console.log(expenses)
-    }
+  const expenceCreateHandler = (event) => {
+    event.preventDefault();
+    const expenses = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate),
+    };
+   props.onSaveExpenseData(expenses);
+    setEnteredTitle("");
+    setEnteredAmount("");
+    setEnteredDate("");
+  };
 
-// const expenceCreate = (event) => {
-//   event.preventDefault()
-// const object = {title:event.target.title.value,amount:event.target.amount.value,date:event.target.date.value}
-// console.log(object)
-// }
-
-
-
-
+  // const expenceCreate = (event) => {
+  //   event.preventDefault()
+  // const object = {title:event.target.title.value,amount:event.target.amount.value,date:event.target.date.value}
+  // console.log(object)
+  // }
 
   return (
-   <form onSubmit={expenceCreate}>
-    <div className='new-expense__controls'>
-      <div className='new-expense__control'>
-      <label >Expense title:</label>
-      <input type='text' id="title" name="title" onChange={titleChangeHandler} />
+    <form onSubmit={expenceCreateHandler}>
+      <div className="new-expense__controls">
+        <div className="new-expense__control">
+          <label>Expense title:</label>
+          <input
+            type="text"
+            id="title"
+            name="title"
+            value={enteredTitle}
+            onChange={titleChangeHandler}
+          />
+        </div>
+        <div className="new-expense__control">
+          <label>Expense Amount:</label>
+          <input
+            type="number"
+            id="amount"
+            name="amount"
+            value={enteredAmount}
+            onChange={amountChangeHandler}
+          />
+        </div>
+        <div className="new-expense__control">
+          <label>Date:</label>
+          <input
+            type="date"
+            id="date"
+            name="date"
+            value={enteredDate}
+            onChange={dateChangeHandler}
+          />
+        </div>
       </div>
-      <div className='new-expense__control'>
-      <label >Expense Amount:</label>
-      <input type='number' id="amount" name="amount" onChange={amountChangeHandler} />
-      </div>
-      <div className='new-expense__control'>
-      <label >Date:</label>
-      <input type='date' id="date" name="date" onChange={dateChangeHandler} />
-      </div>
-      </div>
-      <div className='new-expense__actions'>
-      <button type='submit'>Add Expence</button>
+      <div className="new-expense__actions">
+        <button type="submit">Add Expence</button>
       </div>
     </form>
-   
   );
 }
